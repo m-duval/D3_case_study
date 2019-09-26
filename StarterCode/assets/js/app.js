e2f9fe6e01c55b9870e4626a9ad44f227a9ae4d6
MICHAEL DUVAL <duv74@hotmail.com>
2019-09-26 15:26:11 -0400

    update work

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,31 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 660;
+
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+  };
+
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+  d3.csv("data.csv", function(error, povertyData) {
+    if (error) return console.warn(error);
+    
+    console.log(povertyData);
+    
+    povertyData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.obesity = +data.obesity;
+    });
