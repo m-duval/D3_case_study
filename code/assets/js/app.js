bf9f847aa2783fdd9e56ccab915462aa98f71ca8
MICHAEL DUVAL <duv74@hotmail.com>
2019-10-03 14:27:28 -0400

    update

--- code/assets/js/app.js
+++ code/assets/js/app.js
@@ -1,24 +1,24 @@
+// CHART STRUCTURE
+
 var svgWidth = 960;
+
 var svgHeight = 660;
 
-var margin = {
-    top: 30,
-    right: 30,
-    bottom: 30,
-    left: 30
-  };
+var margin = {top: 30, right: 30, bottom: 100, left: 100};
 
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
-var svg = d3
-  .select("body")
+// SVG WRAPPER
+
+var svg = d3.select("body")
   .append("svg")
   .attr("height", svgHeight)
-  .attr("width", svgWidth);
+  .attr("width", svgWidth)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var chartGroup = svg.append("g");
 
 // IMPORT CSV DATA
 
@@ -32,30 +32,79 @@ console.log(povertyData);
       data.obesity = +data.obesity;
     });
 
+// INITIALIZE TOOL TIP
+
+    var toolTip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip");
+
+// CREATE TOOL TIP
+
+    chart.call(toolTip);
+
 // CREATE SCALES
 
-    var xLinearScale = d3.scaleLinear().range([0, width]);
-    var yLinearScale = d3.scaleLinear().range([height, 0]);
+    var xScale = d3.scaleLinear().range([0, width]);
+    var yScale = d3.scaleLinear().range([height, 0]);
 
 // CREATE AXES
 
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+// STORE MIN + MAX VALUES
+
+    var xMin;
+    var xMax;
+    var yMin;
+    var yMax;
+
+// FUNCTION TO FIND MIN + MAX
+
+    function findMinAndMaxX(dataColumnX) {
+        xMin = d3.min(dataset, function (d) { return d[dataColumnX] * 0.8 });
+        xMax = d3.max(dataset, function (d) { return d[dataColumnX] * 1.2 });
+    };
+
+    function findMinAndMaxY(dataColumnY) {
+        yMin = d3.min(dataset, function (d) { return d[dataColumnY] * 0.8 });
+        yMax = d3.max(dataset, function (d) { return d[dataColumnY] * 1.2 });
+    };
+
+// SET DEFAULT X-AXIS
+
+    var defaultAxisLabelX = "POVERTY"
+
+// SET DEFAULT Y-AXIS
+
+    var defaultAxisLabelY = "OBESITY"
+
+// CALL FINDMINANDMAX
+
+    findMinAndMaxX(defaultAxisLabelX)
+    findMinAndMaxY(defaultAxisLabelY)
+
+// SET AXES DOMAIN
+
+    xScale.domain([xMin, xMax]);
+    yScale.domain([yMin, yMax]);
 
 // APPEND AXES
 
-chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(bottomAxis);
+    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(bottomAxis);
 
-chartGroup.append("g").call(leftAxis);
+    chartGroup.append("g").call(leftAxis);
 
 // APPEND AXES TO CHART
 
-chartGroup.append("g")
-    .attr("transform", 'translate(0, ${height})')
-    .call(bottomAxis);
+    chartGroup.append("g")
+        .attr("transform", 'translate(0, ${height})')
+        .attr("class", "x-axis")
+        .call(xAxis);
 
-chartGroup.append("g")
-    .call(leftAxis);
+    chartGroup.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
 
 // APPEND CIRCLES TO DATA POINTS
 
@@ -67,12 +116,7 @@ var circlesGroup = chartGroup.selectAll("circle")
     .attr("cy", d => yScale(d))
     .attr("r", "5")
     .attr("fill", "green")
-    .attr("opacity", .5)
-
-// INITIALIZE TOOL TIP
-
-var toolTip = d3.select("body").append("div")
-    .attr("class", "tooltip");
+    .attr("opacity", .5);
 
 // MAKE RESPONSIVE
 
