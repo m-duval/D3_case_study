15abc223e6ea5ead1a6194fbd24f6212c0b50cb3
MICHAEL DUVAL <duv74@hotmail.com>
2019-10-03 14:44:57 -0400

    clean

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-
 @app.route('/data')
 def data():
 
-    df = pd.read_csv('data/data.csv')
+    df = pd.read_csv('data.csv')
     return jsonify(df.to_dict(orient="records"))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
